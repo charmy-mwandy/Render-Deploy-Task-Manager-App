58866530f1e04cff32c9f75d091c7adf44ffb192
Charmy developer <charmainemwandiringa@gmail.com>
2024-10-21 17:06:28 +0200

    update database

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -36,7 +36,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default.os.environ['DATABASE_URL'],
-        conn_max_age-600
+        default = os.environ['DATABASE_URL'],
+        conn_max_age=600
     )
 }
