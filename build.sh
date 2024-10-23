ac38b02cba67c4bd35253d7093ddec81dad2c4c7
Charmy developer <charmainemwandiringa@gmail.com>
2024-10-23 11:14:28 +0200

    updated build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ set -o errexit
 pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
+
+if [[$CREATE_SUPERUSER]];
+then 
+    python3 manage.py createsuperuser --no-input
+fi
